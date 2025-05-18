126918359aa19275f5c651105163712478136ad8
z159423 <tmdghks992@naver.com>
2025-05-19 01:46:49 +0900

    GameScene들 추가

--- Assets/01.Scripts/UI/Scene/UI_GameScene_Home.cs
+++ Assets/01.Scripts/UI/Scene/UI_GameScene_Home.cs
@@ -1,18 +1,129 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEditor;
 
-public class UI_GameScene_Home : MonoBehaviour
+public class UI_GameScene_Home : UI_Scene
 {
-    // Start is called before the first frame update
-    void Start()
+
+    enum Buttons
     {
-        
+        ShopBtn,
+        CharacterBtn,
+        HomeBtn,
+        BoostBtn,
+        QeustBtn
     }
 
-    // Update is called once per frame
-    void Update()
+    enum Texts
     {
-        
+        GemText,
+        TicketCount
+    }
+
+    bool _init = false;
+
+    public LowerBtn selectedLowerBtn;
+
+    public override void Init()
+    {
+        base.Init();
+        Managers.UI.SetCanvas(gameObject, true);
+
+        if (!_init)
+        {
+            FirstSetting();
+        }
+
+        GetButton(Buttons.HomeBtn).GetComponent<LowerBtn>().Select();
+        selectedLowerBtn = GetButton(Buttons.HomeBtn).GetComponent<LowerBtn>();
+    }
+
+    public void FirstSetting()
+    {
+        _init = true;
+
+        Bind<Button>(typeof(Buttons));
+        Bind<TextMeshProUGUI>(typeof(Texts));
+
+        GetButton(Buttons.ShopBtn).GetComponent<LowerBtn>().Init();
+        GetButton(Buttons.CharacterBtn).GetComponent<LowerBtn>().Init();
+        GetButton(Buttons.HomeBtn).GetComponent<LowerBtn>().Init();
+        GetButton(Buttons.BoostBtn).GetComponent<LowerBtn>().Init();
+        GetButton(Buttons.QeustBtn).GetComponent<LowerBtn>().Init();
+
+        GetButton(Buttons.ShopBtn).AddButtonEvent(() =>
+        {
+            var lowerbtn = GetButton(Buttons.ShopBtn).GetComponent<LowerBtn>();
+
+            if (selectedLowerBtn == lowerbtn)
+                return;
+
+            lowerbtn.Select();
+
+            selectedLowerBtn.UnSelect();
+            selectedLowerBtn = lowerbtn;
+        });
+
+        GetButton(Buttons.CharacterBtn).onClick.AddListener(() =>
+        {
+            var lowerbtn = GetButton(Buttons.CharacterBtn).GetComponent<LowerBtn>();
+
+            if (selectedLowerBtn == lowerbtn)
+                return;
+
+            lowerbtn.Select();
+
+            selectedLowerBtn.UnSelect();
+            selectedLowerBtn = lowerbtn;
+        });
+
+        GetButton(Buttons.HomeBtn).AddButtonEvent(() =>
+        {
+            var lowerbtn = GetButton(Buttons.HomeBtn).GetComponent<LowerBtn>();
+
+            if (selectedLowerBtn == lowerbtn)
+                return;
+
+            lowerbtn.Select();
+
+            selectedLowerBtn.UnSelect();
+            selectedLowerBtn = lowerbtn;
+        });
+
+        GetButton(Buttons.BoostBtn).AddButtonEvent(() =>
+        {
+            var lowerbtn = GetButton(Buttons.BoostBtn).GetComponent<LowerBtn>();
+
+            if (selectedLowerBtn == lowerbtn)
+                return;
+
+            lowerbtn.Select();
+
+            selectedLowerBtn.UnSelect();
+            selectedLowerBtn = lowerbtn;
+        });
+
+        GetButton(Buttons.QeustBtn).AddButtonEvent(() =>
+        {
+
+        });
+    }
+
+    public override void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public override void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void SelectLowerBtn()
+    {
+
     }
 }
---
+++ Assets/01.Scripts/UI/Scene/UI_GameScene_Loading.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using DG.Tweening;
+
+public class UI_GameScene_Loading : UI_Scene
+{
+    enum Images
+    {
+        Title2,
+
+    }
+
+    enum Texts
+    {
+        LoadingText
+    }
+
+    bool _init = false;
+
+    public override void Init()
+    {
+        base.Init();
+        Managers.UI.SetCanvas(gameObject, true);
+
+        if (!_init)
+        {
+            FirstSetting();
+        }
+
+        StartCoroutine(Loading());
+
+        IEnumerator Loading()
+        {
+            var titleImage = GetImage(Images.Title2);
+            titleImage.DOFade(1.5f, 1f)
+                .SetLoops(2, LoopType.Yoyo)
+                .SetEase(Ease.InOutSine);
+
+            yield return new WaitForSeconds(2f);
+
+            var loadingText = GetTextMesh(Texts.LoadingText);
+            loadingText.gameObject.SetActive(true);
+            loadingText.DOFade(1f, 1f)
+                .SetEase(Ease.InOutSine);
+
+            yield return new WaitForSeconds(1f);
+
+            var scene = Managers.UI.ShowSceneUI<UI_GameScene_Home>();
+            scene.Init();
+        }
+    }
+
+    public void FirstSetting()
+    {
+        _init = true;
+
+        Bind<Image>(typeof(Images));
+        Bind<TextMeshProUGUI>(typeof(Texts));
+    }
+
+    public override void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public override void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
